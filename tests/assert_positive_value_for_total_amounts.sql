990b1e40a4ca76439ab3d761d5fe0865f2a2a7e1
JuanCarbonell <juanpablo.carbonell@nimbusintelligence.com>
2025-04-29 16:22:31 +0200

    Se corrigio el codigo

--- tests/assert_positive_value_for_total_amounts.sql
+++ tests/assert_positive_value_for_total_amounts.sql
@@ -1,8 +1,6 @@
 select
-    ID AS PAYMENT_ID, 
-    ORDERID as ORDER_ID, 
-    PAYMENTMETHOD AS PAYMENT_METHOD, 
-    STATUS, 
-    AMOUNT / 100 AS AMOUNT, 
-    CREATED AS CREATED_AT
-from {{ source('stripe', 'payment') }}
+  order_id,
+  sum(amount) as total_amount
+from {{ ref('stg_stripe__payments') }}
+group by 1
+having (total_amount < 0)
